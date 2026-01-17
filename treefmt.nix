5089bf685b3212a69178cb23077baa2b4c19dfd6
Justin Hoang <72476123+suasuasuasuasua@users.noreply.github.com>
2026-01-16 18:02:54 -0700

    Shell/add cmake format (#6)
    * docs(math): add docstrings to existing functions
    
    * fix(shell): use clang21 stdenv
    
    * build(treefmt): add cmake-format
    
    * style(cmake): run nix fmt

--- treefmt.nix
+++ treefmt.nix
@@ -7,6 +7,7 @@
   };
 
   programs = {
+    cmake-format.enable = true;
     clang-format.enable = true;
     nixfmt.enable = pkgs.lib.meta.availableOn pkgs.stdenv.buildPlatform pkgs.nixfmt-rfc-style.compiler;
     nixfmt.package = pkgs.nixfmt-rfc-style;
